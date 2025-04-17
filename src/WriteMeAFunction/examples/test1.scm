bbb255aa21f4acebfc1fa2849840e4e1715bf2d1
Trey Wright <treylily@gmail.com>
2025-04-17 11:14:29 -0500

    Delete src/WriteMeAFunction/examples/test1.scm

--- src/WriteMeAFunction/examples/test1.scm
+++
@@ -1,8 +0,0 @@
-Write me a function called TEST {
-    Tell them "Welcome to the Number Guessing Game!"?
-    Tell them "I'm thinking of a number between 1 and 100"?
-    Tell them "You have 5 attempts to guess it"?
-    Tell them "Let's begin!"?
-}
-
-Can it TEST?
