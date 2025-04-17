fd78bdeabc5e6b34b7d33aa9ef8853bd9a3f9695
Trey Wright <treylily@gmail.com>
2025-04-17 10:45:49 -0500

    Update test2.scm

--- src/WriteMeAFunction/examples/test2.scm
+++ src/WriteMeAFunction/examples/test2.scm
@@ -1,13 +1,13 @@
 Tell them "Welcome to the Number Guessing Game!"?
-Tell them "I'm thinking of a number between 1 and 100"?
+Tell them "I'm thinking of a number between 0 and 99"?
 Tell them "You have 5 attempts to guess it"?
 Tell them "Let's begin!"?
 
-Can you make target = ROLL * 17
+Can you make target = RAND
 Can you make attempts = 5
 Can you make guessed = 0
 
-Write me a function called TEST {
+Write me a function called TOO_LOW {
     Tell them "Too low!"?
 }
 
@@ -21,7 +21,7 @@ Can you do until (attempts > 0 && guessed == 0) {
     }
 
     Will it guess < target? {
-        Can it TEST?
+        Can it TOO_LOW?
     }
 
     Will it guess == target? {
